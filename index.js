f4707cb4b0ca7c733f24b01c8eb6cf918e4e4822
Pablo Bajo <haploide1998@hotmail.com>
2020-01-28 20:50:37 -0300

    public access configuration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {Application, genesisBlockDevnet, configDevnet} = require('lisk-sdk');
 const HelloTransaction = require('./hello_transaction');
 
 configDevnet.app.label = 'hello-world-blockchain-app';
+configDevnet.modules.http_api.access.public = true;
 
 const app = new Application(genesisBlockDevnet, configDevnet);
 app.registerTransaction(HelloTransaction);
